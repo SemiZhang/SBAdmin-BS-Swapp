33a68abb59f565bb24a46c91fe11532314d3af31
SemiZhang <shunyang.zhang@epfedu.fr>
2022-03-11 18:03:11 +0100

    update slider hook method

--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,16 @@
-var slider = document.getElementById("RangeInput");
-var output = document.getElementById("RangeOutput");
+var slider = document.getElementById("RangeInput_Largeur");
+var output = document.getElementById("RangeOutput_Largeur");
 output.innerHTML = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
+slider.addEventListener("input",ValueChanged);
+function ValueChanged() {
     output.innerHTML = this.value;
 }
+
+// slider.oninput(function() {
+//     output.innerHTML = this.value;
+// })
+
+var tag_slider = document.getElementsByName("RangeInput");
+var tag_output = document.getElementsByName("RangeOutput");
