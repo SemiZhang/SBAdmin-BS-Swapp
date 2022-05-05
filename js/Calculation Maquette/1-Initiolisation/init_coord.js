5ffb5b10e5603d79ac56c43ebc04998b1e54a38a
SemiZhang <shunyang.zhang@epfedu.fr>
2022-05-05 17:22:25 +0200

    Initial Patient data

--- js/Calculation Maquette/1-Initiolisation/init_coord.js
+++ js/Calculation Maquette/1-Initiolisation/init_coord.js
@@ -39,3 +39,34 @@ let reglage = {
         },
     },
 };
+
+let taille = 175;
+let patient = {
+    taille: taille,
+    Lpied: 0.146*taille,
+    Ltibia: 0.246*taille,
+    Lcuisse: 0.245*taille,
+    Lbassin: 0.174*taille,
+    Htronc: 0.34*taille,
+    Lepaules: 0.259*taille,
+    Ltete:0.130*taille,
+    Lbras:0.136*taille,
+    LavtBras:0.146*taille,
+    Lmain:1/3*0.108*taille,
+
+    lbassin: 0.1*taille,
+    ltronc: 0.15*taille,
+}
+patient.ltete = 3/4*patient.Ltete;
+patient.lpien = 1/2*patient.ltete;
+
+patient.lcuisse = 0.3*patient.Lcuisse;
+patient.ltibia = 0.3*patient.Ltibia;
+patient.lbras = 0.4*patient.Lbras;
+patient.lavtBras = 0.4*patient.LavtBras;
+
+patient.ptronc = 1/3*patient.Htronc;
+patient.pbras = patient.lbras;
+patient.pavtBras = patient.lavtBras;
+
+patient.pointure = Math.ceil((patient.Lpied+1)*3/2);
