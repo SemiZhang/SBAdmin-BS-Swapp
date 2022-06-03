d2a1811c3c2fda2af4faa3469c627a01ca5054ba
SemiZhang <zsy455461505@gmail.com>
2022-06-03 10:18:25 +0200

    Project structure update

--- js/1_Initialisation/init_slider.js
+++ js/1_Initialisation/init_slider.js
@@ -13,25 +13,33 @@ function ValueChanged() {
     Plotly.react('myPloty3DChart', data, layout[0], layout[1]);
 }*/
 
+// reglage = require("/js/Calculation_Maquette/1_Initialisation/init_coord.js");
+// require("/js/Calculation_Maquette/1_Initialisation/init_global.js")
+
+
+// import {init_data} from "/js/1_Initialisation/init_global.js";
+// import {init_layout} from "/js/1_Initialisation/init_global.js";
+// import {init_global} from "/js/1_Initialisation/init_global.js";
 
 //todo: use loop to bind
-allSliders = Array();
-for (let i1 in reglage) {
-    for (let i2 in reglage[i1]){
-        for (let i3 in reglage [i1][i2]){
+let allSliders = Array();
+for (let i1 in reglage_chair) {
+    for (let i2 in reglage_chair[i1]){
+        for (let i3 in reglage_chair [i1][i2]){
             let slider = document.getElementById('RangeInput_'+i1+'_'+i2+'_'+i3);
             let output = document.getElementById('RangeOutput_'+i1+'_'+i2+'_'+i3);
             try{
-                slider.value = reglage[i1][i2][i3];
+                slider.value = reglage_chair[i1][i2][i3];
                 output.innerHTML = slider.value;
                 slider.addEventListener("input",ValueChanged);
                 function ValueChanged() {
                     output = document.getElementById('RangeOutput_'+i1+'_'+i2+'_'+i3);
                     output.innerHTML = this.value;
-                    reglage[i1][i2][i3] = this.value;
-                    let data = init_data();
-                    let layout = init_layout();
-                    Plotly.react('myPloty3DChart', data, layout[0], layout[1]);
+                    reglage_chair[i1][i2][i3] = this.value;
+                    init_global();
+                    // let data = init_data();
+                    // let layout = init_layout();
+                    // Plotly.react('myPloty3DChart', data, layout[0], layout[1]);
                 }
             } catch(error) {
                 console.log(error)
