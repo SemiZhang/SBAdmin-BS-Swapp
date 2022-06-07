4eef735f2275209d440d063d6fdea0a89efb4094
SemiZhang <zsy455461505@gmail.com>
2022-06-07 14:54:04 +0200

    Reformat variables

--- js/1_Initialisation/init_global.js
+++ js/1_Initialisation/init_global.js
@@ -2,101 +2,100 @@
 // import {reglage_patient} from "/js/1_Initialisation/init_coord.js";
 // import {patient} from "/js/1_Initialisation/init_coord.js";
 
-let reglage_chair = {
-    siege: {
-        assise:{
-            largeur: 35,
-            profondeur: 40,
-            hauteur: 45,
-            angle: 10,
-            distanceFourche: 0,
-        },
-        dossier:{
-            largeur: 40,
-            hauteur: 20,
-            angle: 90,
-        },
-        potence:{
-            longueur: 35,
-            angle: 90,
-        },
-        repose:{
-            longueur: 15,
-            largeur: 5,
-            angle: 90,
-        }
-    },
-    roue: {
-        avt: {
-            deport: 50,
-            voie: 18,
-            rayon: 5,
-            carrosage: 0,
-        },
-        arr: {
-            deport: 0,
-            voie: 20,
-            rayon: 30,
-            carrosage: 0,
-            MC_distance: 3.5,
-            MC_rayon: 28,
-        },
-    },
-};
-
-let reglage_patient = {
-    bras:{
-        angle: 45,
-    },
-    main:{
-        angle: 0,
-    }
-}
-
-let taille = 175;
-let patient = {
-    taille: taille,
-    Lpied: 0.146*taille,
-    Ltibia: 0.246*taille,
-    Lcuisse: 0.245*taille,
-    Lbassin: 0.174*taille,
-    Htronc: 0.34*taille,
-    Lepaule: 0.259*taille,
-    Ltete:0.130*taille,
-    Lbras:0.136*taille,
-    LavtBras:0.146*taille,
-    Lmain:1/3*0.108*taille,
-
-    lbassin: 0.1*taille,
-    ltronc: 0.15*taille,
-}
-patient.ltete = 3/4*patient.Ltete;
-patient.lpied = 1/2*patient.ltete;
-
-patient.lcuisse = 0.3*patient.Lcuisse;
-patient.ltibia = 0.3*patient.Ltibia;
-patient.lbras = 0.4*patient.Lbras;
-patient.lavtBras = 0.4*patient.LavtBras;
-
-patient.ptronc = 1/3*patient.Htronc;
-patient.pbras = patient.lbras;
-patient.pavtBras = patient.lavtBras;
-
-patient.pointure = Math.ceil((patient.Lpied+1)*3/2);
-
-let repere_chair = Array();
-let points_chair = Array();
-let repere_patient={};
-let data = Array();
+init_coord();
+init_layout();
 init_global();
+init_slider();
+
+function init_coord(){
+    reglage_chair = {
+        siege: {
+            assise:{
+                largeur: 35,
+                profondeur: 40,
+                hauteur: 45,
+                angle: 10,
+                distanceFourche: 0,
+            },
+            dossier:{
+                largeur: 40,
+                hauteur: 20,
+                angle: 90,
+            },
+            potence:{
+                longueur: 35,
+                angle: 90,
+            },
+            repose:{
+                longueur: 15,
+                largeur: 5,
+                angle: 90,
+            }
+        },
+        roue: {
+            avt: {
+                deport: 50,
+                voie: 18,
+                rayon: 5,
+                carrosage: 0,
+            },
+            arr: {
+                deport: 0,
+                voie: 20,
+                rayon: 30,
+                carrosage: 0,
+                MC_distance: 3.5,
+                MC_rayon: 28,
+            },
+        },
+    };
+    reglage_patient = {
+        bras:{
+            angle: 45,
+        },
+        main:{
+            angle: 0,
+        }
+    }
+
+    let taille = 175;
+    patient = {
+        taille: taille,
+        Lpied: 0.146*taille,
+        Ltibia: 0.246*taille,
+        Lcuisse: 0.245*taille,
+        Lbassin: 0.174*taille,
+        Htronc: 0.34*taille,
+        Lepaule: 0.259*taille,
+        Ltete:0.130*taille,
+        Lbras:0.136*taille,
+        LavtBras:0.146*taille,
+        Lmain:1/3*0.108*taille,
+
+        lbassin: 0.1*taille,
+        ltronc: 0.15*taille,
+    }
+    patient.ltete = 3/4*patient.Ltete;
+    patient.lpied = 1/2*patient.ltete;
+
+    patient.lcuisse = 0.3*patient.Lcuisse;
+    patient.ltibia = 0.3*patient.Ltibia;
+    patient.lbras = 0.4*patient.Lbras;
+    patient.lavtBras = 0.4*patient.LavtBras;
+
+    patient.ptronc = 1/3*patient.Htronc;
+    patient.pbras = patient.lbras;
+    patient.pavtBras = patient.lavtBras;
+
+    patient.pointure = Math.ceil((patient.Lpied+1)*3/2);
+}
 
 function init_global() {
     data = Array();
-    init_data();
-    let layout = init_layout();
-    Plotly.react('myPloty3DChart', data, layout[0], layout[1]);
+    init_data()
+    // let layout = init_layout();
+    Plotly.react('myPloty3DChart', data, plotly_layout, plotly_config);
 }
-
 
 // Fonction principale
 
@@ -466,12 +465,11 @@ function cal_mesh_chair() {
 }
 
 function cal_repere_patient() {
+    repere_patient = {};
     // Repere Patient
     let thetaD = reglage_chair.siege.dossier.angle;
     let thetaBras = reglage_patient.bras.angle;
     let thetaP = reglage_chair.siege.potence.angle;
-
-
 
     let MP_R0_Rassise_construction = repere_chair.siege.assise;
     let rotx_90 = matrice_Rotation('x',90,4);
@@ -1501,7 +1499,7 @@ function matrice_Translation(x,y,z) {
 // Config Plotly
 
 function init_layout() {
-    var layout = {
+    plotly_layout = {
         margin: {t: 0, l: 0, b: 0, r: 0},
         autosize: true, // set autosize to rescale
         automargin: true,
@@ -1553,11 +1551,11 @@ function init_layout() {
         },
     };
 
-    var config = {
+    plotly_config = {
         responsive: true,
     };
 
-    return [layout, config];
+    return [plotly_layout, plotly_config];
 }
 
 
@@ -1628,4 +1626,4 @@ function init_slider() {
         }
     }
 }
-init_slider()
+
